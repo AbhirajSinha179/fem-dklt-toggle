f00982afc32a5be910243e2ff71edd849f41511a
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2023-06-16 16:38:23 +0530

    update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,60 +1,58 @@
 
+/*
+The first time the page is loaded, the color mode set on the preference
+is used and set as 'default' in the local storage.
+Changing the default preferences works the same way as changing the
+color mode using the buttons, if the page is loaded.
+When the page is reloaded, whatever is the value set on the local storage
+has precedence over the values in the preference. If the preference
+changed after the page was visited - and the page is not loaded -
+the last value saved on the local storage is loaded.
+*/
 
-const darkButton=document.getElementById('dark');
-const lightButton=document.getElementById('light');
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
 
-const setColorMode=()=>{
-    if(localStorage.getItem('colorMode')=='light'){
-        setLightMode();
-        lightButton.click();
-    }
-    else{
-        setDarkMode();
-        darkButton.click();
-    }
+const setDarkMode = () => {
+    document.querySelector('body').classList = 'dark';
+    localStorage.setItem('colorMode', 'dark');
 };
 
-const checkMode=()=>{
-    if(localStorage.getItem('colorMode')==null) {
-        if (window.matchMedia("(prefers-color-scheme:light)").matches) {
-            lightButton.click();
-        } else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
-            darkButton.click();
-        }
-    }
+const setLightMode = () => {
+    document.querySelector('body').classList = 'light';
+    localStorage.setItem('colorMode', 'light');
 };
 
-const setDarkMode=()=>{
-    document.querySelector('body').classList='dark';
-}
-const setLightMode=()=>{
-    document.querySelector('body').classList='light';
-}
+const colorModeFromLocalStorage = () => {
+    return localStorage.getItem('colorMode');
+};
 
-const checkModeChange=()=>{
+const colorModeFromPreferences = () => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light' // If preference is set or does not match anything (light is default)
+};
 
-}
-setColorMode();
-checkMode();
-checkModeChange();
+const loadAndUpdateColor = () => {
+    // local storage has precendence over the prefers-color-scheme
+    const color = colorModeFromLocalStorage() || colorModeFromPreferences();
+    color == 'dark' ? darkButton.click() : lightButton.click();
+};
 
-const radioButtons= document.querySelectorAll('.toggle__wrapper input');
-for(let i=0;i<radioButtons.length;i++){
-    radioButtons[i].addEventListener('click', event=>{
-    if(darkButton.checked) {
-        document.querySelector('body').classList = 'dark'
-        setDarkMode();
-    }
-    else{
-        document.querySelector('body').classList='light';
-        setLightMode();
-    }
-
+// when the inputs are clicked, check which radio button is checked and change the color
+const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+radioButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        darkButton.checked ? setDarkMode() : setLightMode();
     });
-}
+});
 
-const checkModeChange=()=>{
-    window.matchMedia("(prefers-color-scheme:light)").addEventListener('change',(event)=>{
-        checkMode();
+// when the prefers-color-scheme changes, this event will be emitted
+// event reflects the media query, if it matches, the new color is dark, else it is light
+window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (event) => {
+        event.matches ? darkButton.click() : lightButton.click();
     });
-};
+
+// Load the right color on startup - localStorage has precedence
+loadAndUpdateColor();
